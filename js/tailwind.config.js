3720dc7a84c3bad35e58f0dd7ba6a2e643f49c32
shanto <shantohasibulhasan379@gmail.com>
2024-10-13 20:42:48 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Heading Color 
+        colors: {
+          white: '#FFFFFF',
+          iconColor: '#FFF84E',
+          bgColor: '#000000',
+        },
+                // Heading Color End
+        // container 
+
+        spacing: {
+          'headerContainer': '1450px',
+        },
+
+        // container End
+        // banner images 
+
+        backgroundImage: {
+          'banner': "url('images/banner (2).png')",
+        },
+
+        // banner images End
+
+        // Google Font 
+        fontFamily: {
+          'inter': ['Inter',],
+        }
+        // Google Font End
+      }
+    }
+  }
